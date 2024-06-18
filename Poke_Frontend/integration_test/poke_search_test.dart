0bd35475fcd9f65597f48012a80a04c99c43a496
Katharina <katharina.winnen@stud.th-rosenheim.de>
2024-06-18 11:55:08 +0200

    e2e test scenarios

--- Poke_Frontend/integration_test/poke_search_test.dart
+++ Poke_Frontend/integration_test/poke_search_test.dart
@@ -7,7 +7,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter',
+    testWidgets('A valid Pokémon name is being used as input.',
             (tester) async {
           // Load app widget.
           await tester.pumpWidget(const MyApp());
@@ -18,6 +18,99 @@ void main() {
           expect(find.text('Pokemon name'), findsOneWidget);
           expect(find.text('Search'), findsOneWidget);
 
+          await tester.enterText(find.byKey(const Key('pokemon_input')), 'mew');
+          await tester.pumpAndSettle();
+
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('mew'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+
+          await tester.tap(find.byKey(const Key('search_button')));
+          await tester.pumpAndSettle();
+
+          expect(find.text('Features:'), findsOneWidget);
+          expect(find.text('ID: 11'), findsOneWidget);
+          expect(find.text('Name: mew'), findsOneWidget);
+          expect(find.text('Height: 4'), findsOneWidget);
+          expect(find.text('Order: 248'), findsOneWidget);
+          expect(find.text('Weight: 40'), findsOneWidget);
+    });
+
+    testWidgets('A Pokémon name that doesnt exist is being used as input.',
+            (tester) async {
+          // Load app widget.
+          await tester.pumpWidget(const MyApp());
+
+          // Verify that the initial page is SelectServerPage
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('Pokemon name'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+
+          await tester.enterText(find.byKey(const Key('pokemon_input')), 'bikachu');
+          await tester.pumpAndSettle();
+
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('bikachu'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+
+          await tester.tap(find.byKey(const Key('search_button')));
+          await tester.pumpAndSettle();
+
+          expect(find.text('Features:'), findsOneWidget);
+          expect(find.text('Error: Exception: Pokémon could not be found!'), findsOneWidget);
+        });
+
+    testWidgets('A Pokémon name with an invalid character is given as input.',
+            (tester) async {
+          // Load app widget.
+          await tester.pumpWidget(const MyApp());
+
+          // Verify that the initial page is SelectServerPage
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('Pokemon name'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+
+          await tester.enterText(find.byKey(const Key('pokemon_input')), 'mr. mime');
+          await tester.pumpAndSettle();
+
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('mr. mime'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+
+          await tester.tap(find.byKey(const Key('search_button')));
+          await tester.pumpAndSettle();
+
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('mr. mime'), findsOneWidget);
+          expect(find.text('Invalid Pokémon name. Only letters, numbers, and hyphens are allowed!'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+        });
+
+    testWidgets('No input is being given.',
+            (tester) async {
+          // Load app widget.
+          await tester.pumpWidget(const MyApp());
+
+          // Verify that the initial page is SelectServerPage
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('Pokemon name'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
+
+          await tester.tap(find.byKey(const Key('search_button')));
+          await tester.pumpAndSettle();
+
+          expect(find.text('Whos that Pokémon?'), findsOneWidget);
+          expect(find.text('Enter the name of the Pokémon:'), findsOneWidget);
+          expect(find.text('Pokemon name'), findsOneWidget);
+          expect(find.text('Please enter the name of a Pokémon!'), findsOneWidget);
+          expect(find.text('Search'), findsOneWidget);
         });
   });
 }

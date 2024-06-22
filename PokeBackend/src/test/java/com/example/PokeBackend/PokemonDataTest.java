6c09c14e789ccce8907c06b9eb224786de980760
Katharina <katharina.winnen@stud.th-rosenheim.de>
2024-06-22 14:52:21 +0200

    sonar open issues

--- PokeBackend/src/test/java/com/example/PokeBackend/PokemonDataTest.java
+++ PokeBackend/src/test/java/com/example/PokeBackend/PokemonDataTest.java
@@ -2,11 +2,11 @@ package com.example.PokeBackend;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.LinkedHashMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PokemonDataTest {
 
@@ -18,67 +18,43 @@ class PokemonDataTest {
     }
 
     @Test
-    void testGetId() {
+    void testId() {
+        // Test setId and getId
         pokemonData.setId(1);
         assertEquals(1, pokemonData.getId());
     }
 
     @Test
-    void testSetId() {
-        pokemonData.setId(1);
-        assertEquals(1, pokemonData.getId());
-    }
-
-    @Test
-    void testSetName() {
+    void testName() {
+        // Test setName and getName
         pokemonData.setName("Pikachu");
         assertEquals("Pikachu", pokemonData.getName());
     }
 
     @Test
-    void testGetName() {
-        pokemonData.setName("Pikachu");
-        assertEquals("Pikachu", pokemonData.getName());
-    }
-
-    @Test
-    void testSetHeight() {
+    void testHeight() {
+        // Test setHeight and getHeight
         pokemonData.setHeight(10);
         assertEquals(10, pokemonData.getHeight());
     }
 
     @Test
-    void testGetHeight() {
-        pokemonData.setHeight(10);
-        assertEquals(10, pokemonData.getHeight());
-    }
-
-    @Test
-    void testSetOrder() {
+    void testOrder() {
+        // Test setOrder and getOrder
         pokemonData.setOrder(1);
         assertEquals(1, pokemonData.getOrder());
     }
 
     @Test
-    void testGetOrder() {
-        pokemonData.setOrder(1);
-        assertEquals(1, pokemonData.getOrder());
-    }
-
-    @Test
-    void testSetWeight() {
+    void testWeight() {
+        // Test setWeight and getWeight
         pokemonData.setWeight(60);
         assertEquals(60, pokemonData.getWeight());
     }
 
     @Test
-    void testGetWeight() {
-        pokemonData.setWeight(60);
-        assertEquals(60, pokemonData.getWeight());
-    }
-
-    @Test
-    void testSetSprites() {
+    void testSprites() {
+        // Test setSprites and getSprites
         LinkedHashMap<String, String> sprites = new LinkedHashMap<>();
         sprites.put("front_default", "http://example.com/front.png");
         pokemonData.setSprites(sprites);
@@ -86,34 +62,9 @@ class PokemonDataTest {
     }
 
     @Test
-    void testGetSprites() {
-        LinkedHashMap<String, String> sprites = new LinkedHashMap<>();
-        sprites.put("front_default", "http://example.com/front.png");
-        pokemonData.setSprites(sprites);
-        assertEquals(sprites, pokemonData.getSprites());
-    }
-
-    @Test
-    void testGetFrontDefault() {
+    void testFrontDefault() {
+        // Test setFrontDefault and getFrontDefault
         pokemonData.setFrontDefault("http://example.com/front.png");
         assertEquals("http://example.com/front.png", pokemonData.getFrontDefault());
     }
-
-    @Test
-    void testSetFrontDefault() {
-        pokemonData.setFrontDefault("http://example.com/front.png");
-        assertEquals("http://example.com/front.png", pokemonData.getFrontDefault());
-    }
-
-
-    /*
-    @Test
-    void testOnCreate() {
-        PokemonData spyPokemonData = Mockito.spy(pokemonData);
-        spyPokemonData.onCreate();
-        assertNotNull(spyPokemonData.getCreatedAt());
-    }
-
-     */
 }
-
